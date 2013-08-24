acc5c8b474da6bcccdcca728f949ec20908f8f6f
MikeKirschner <mjk.kirschner@gmail.com>
2013-08-23 20:39:07 -0400

    reorganized to use NGUI's event handling to drive vectrosity line drawing and extrusion, can now click on non UI and get events to fall through listener.

---
+++ UIsetup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIsetup : MonoBehaviour {
+
+	public GameObject listener;
+
+	// Use this for initialization
+	void Start () {
+
+
+				UICamera.fallThrough = listener;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ listener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class listener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick() {
+
+				Debug.Log ("OMG");
+
+	}
+
+
+}
